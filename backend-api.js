9ba4c0ad2a8ee07425775680f4ed5507874e26ca
haseeb-techsavy <haseeb.ee12@gmail.com>
2026-02-17 12:37:33 +0500

    updated the backend api and also package.json files

--- backend-api.js
+++ backend-api.js
@@ -12,7 +12,9 @@ app.use(express.json());
 
 // Initialize Firebase Admin
 try {
-    const serviceAccount = require('./serviceAccountKey.json');
+    const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT 
+    ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+    : require('./serviceAccountKey.json');
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount)
     });
